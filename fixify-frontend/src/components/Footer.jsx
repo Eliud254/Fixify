287fcd611d397a039277832f45ad1bc836422e43
eliud <eliudobure@gmail.com>
2024-03-23 23:53:52 +0300

    Footer done

--- fixify-frontend/src/components/Footer.jsx
+++ fixify-frontend/src/components/Footer.jsx
@@ -1,12 +1,41 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-white p-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Fixify. All rights reserved.</p>
+    <footer className="bg-gray-800 text-white">
+      <div className="container mx-auto py-8 px-4">
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full md:w-1/3 lg:w-1/4 mb-8 md:mb-0">
+            <h4 className="text-lg font-semibold mb-4">Quick Links</h4>
+            <ul className="list-none">
+              <li><a href="/about" className="hover:text-gray-400">About Us</a></li>
+              <li><a href="/services" className="hover:text-gray-400">Our Services</a></li>
+              <li><a href="/contact" className="hover:text-gray-400">Contact Us</a></li>
+            </ul>
+          </div>
+          <div className="w-full md:w-1/3 lg:w-1/4 mb-8 md:mb-0">
+            <h4 className="text-lg font-semibold mb-4">Follow Us</h4>
+            <ul className="list-none">
+              <li><a href="#" className="hover:text-gray-400">Facebook</a></li>
+              <li><a href="#" className="hover:text-gray-400">Twitter</a></li>
+              <li><a href="#" className="hover:text-gray-400">Instagram</a></li>
+            </ul>
+          </div>
+          <div className="w-full md:w-1/3 lg:w-1/4 mb-8 md:mb-0">
+            <h4 className="text-lg font-semibold mb-4">Contact Info</h4>
+            <p>123 Street Name</p>
+            <p>City, Country</p>
+            <p>Email: info@fixify.com</p>
+            <p>Phone: +123 456 7890</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-900 text-center py-4">
+        <p>&copy; {new Date().getFullYear()} Fixify. All rights reserved.</p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
